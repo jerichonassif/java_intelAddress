6c4f1d481052f6b15000ad236cef4392444795d2
Ayman Naser Eddin <42473616+AymanNaserEddin@users.noreply.github.com>
2020-07-16 11:44:12 +0300

    .

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,9 +9,11 @@ public class Main {
         InetAddress ip=InetAddress.getByName("activity.qou.edu");
 
         //getHostName() : it returns the host name of the IP address.
+        //getHostName() : تقوم بإرجاع اسم المضيف لعنوان الايبي .
         System.out.println("Host Name: "+ip.getHostName());
 
         //getHostAddress() : it returns the IP address in string format.
+        //getHostAddress() :تقو بارجاع الايبي الخاص بالعنوان التي تم وضعه في المتغير سترينغ
         System.out.println("IP Address: "+ip.getHostAddress());
 
     }
